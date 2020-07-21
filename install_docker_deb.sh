5e7b5013ee6dc65592e473ce1a29faecffca219d
Maxim Peten <mpeten@mindk.com>
2020-07-21 20:24:05 +0300

    update install_docker_deb.sh

--- install_docker_deb.sh
+++ install_docker_deb.sh
@@ -11,6 +11,6 @@ fi
 
 if [ ! -e "/usr/local/bin/docker-compose" ]; then
         echo "installing docker-compose"
-        sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
+        sudo chmod +x /bin/docker-compose
 fi
