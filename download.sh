0778f76a8f0e5ed4a24792ca7435867495feaf5f
Maxim Peten <mpeten@mindk.com>
2020-07-22 22:02:46 +0300

    clean code

--- download.sh
+++ download.sh
@@ -2,6 +2,6 @@
 set -ex; \
 	curl -o wordpress.tar.gz -fSL "https://wordpress.org/wordpress-5.4.1.tar.gz"; \
 	echo "9800c231828eb5cd76ba0b8aa6c1a74dfca2daff *wordpress.tar.gz" | sha1sum -c -; \
-	tar -xzf wordpress.tar.gz -C $PWD; \
+	tar -xzf wordpress.tar.gz -C "$PWD"; \
 	rm wordpress.tar.gz; \
-	chown -R www-data:www-data $PWD/wordpress
+	chown -R www-data:www-data "$PWD/wordpress"
