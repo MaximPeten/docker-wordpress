0bcca90e059b665348804be5d074643e9ca312d7
Maxim Peten <m.maingml@gmail.com>
2020-07-22 13:14:41 +0300

    Delete ssl_renew.sh

--- ssl_renew.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
-
-cd /opt/dockers/wordpress-php-fpm
-$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
-$DOCKER system prune -af
